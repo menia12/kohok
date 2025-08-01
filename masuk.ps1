724f977176c154899c3ec37cf55d5d8d9d56c982
menia12 <160221779+menia12@users.noreply.github.com>
2025-08-01 09:32:41 +0700

    Create masuk.ps1

---
+++ masuk.ps1
@@ -0,0 +1,8 @@
+Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.4.7/CaptchaBotRS.Setup.2.4.7.exe -OutFile CaptchaBotRS.Setup.2.4.7.exe
+copy CaptchaBotRS.Setup.2.4.7.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.4.7.exe
+Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.15.12/Sandboxie-Classic-x64-v5.70.12.exe -OutFile Sandboxie-Classic-x64-v5.70.12.exe
+copy Sandboxie-Classic-x64-v5.70.12.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.70.12.exe
+Invoke-WebRequest https://raw.githubusercontent.com/menia12/sampu/main/berem.txt -OutFile berem.txt
+copy berem.txt C:\Users\Public\Desktop\berem.txt
+Invoke-WebRequest https://freevirtualkeyboard.com/FreeVKSetup.exe -OutFile FreeVKSetup.exe
+copy FreeVKSetup.exe C:\Users\Public\Desktop\FreeVKSetup.exe
